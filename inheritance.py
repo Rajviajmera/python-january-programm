ffc0637057b8f5a3491a8fb6ff4fdca7e130efbf
rajviajmera68-netizen <rajviajmera68@gmail.com>
2026-01-14 12:26:57 +0530

    my programm

--- inheritance.py
+++ inheritance.py
@@ -6,8 +6,25 @@ class shape():
 	def show_dimension(self):
 		print("Length of the shape :",self.length)
 		print("Breadth of the shape :",self.breadth)
+
+
+class shape_3D (shape):
+
+	def __init__(self):
+		super(). __init__()
+		self.height = int(input("Enter Height :"))
+
+	def show_dimension_3 (self) :
+		super(). show_dimension()
+		print ("Height of the shape : ", self.height)
+
+print("====================")
 s1=shape()
 s1.show_dimension()
+print("====================")
 
-s2=shape()
-s2.show_dimension()
+print("====================")
+s2=shape_3D()
+s2.show_dimension_3()
+print("====================")
+print("====================")
